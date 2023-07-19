ec804e26baa9a1ee61f58c6114d875982214b7e7
psycho-o0o <psycho-o0o@gmail.com>
2023-07-19 11:45:53 +0900

    @Design verifyCode 페이지 완성

---
+++ pages/verifyCode.tsx
@@ -0,0 +1,58 @@
+import styled from "styled-components"
+import Button from "@/src/component/Button"
+import Input from "@/src/component/Input"
+import SignLayout from "@/src/component/Layout/Sign"
+
+const InputWrap = styled.div`
+    & > div {
+        margin-bottom: 20px;
+    }
+`
+
+export const BottomWrap = styled.div`
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+`
+
+export const CreateAccountWrap = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    & > a {
+        font-size: 14px;
+        font-weight: bold;
+        color: black;
+        text-decoration: none;
+    }
+    & span {
+        color: #34e0a1;
+    }
+`
+
+const ButtonWrap = styled.div`
+    width: 100%;
+    height: 48px;
+    margin-bottom: 20px;
+`
+
+export default function Verify(): JSX.Element {
+    return (
+        <SignLayout
+            title="VERIFY CODE"
+            subTitle="An authentication code has been sent to your email"
+        >
+            <InputWrap>
+                <Input
+                    placeHolder="Enter Code"
+                    helperText="Didn't receive a code? Resend"
+                />
+            </InputWrap>
+            <BottomWrap>
+                <ButtonWrap>
+                    <Button>VERIFY</Button>
+                </ButtonWrap>
+            </BottomWrap>
+        </SignLayout>
+    )
+}
