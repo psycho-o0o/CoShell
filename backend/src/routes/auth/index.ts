0b74248a62451db7402976e94527089430d5cd93
psycho-o0o <psycho-o0o@gmail.com>
2023-07-29 02:34:21 +0900

    @Feat/backend signUp에 입력값 검증하기

--- backend/src/routes/auth/index.ts
+++ backend/src/routes/auth/index.ts
@@ -1,19 +1,34 @@
 import express, { Router, Request, Response, NextFunction } from "express"
-import mongoose from "mongoose"
 
-const AuthRouter: Router = express.Router()
+const authRouter: Router = express.Router()
 
-AuthRouter.post(
+authRouter.post(
     "/signIn",
     async (req: Request, res: Response, next: NextFunction) => {},
 )
-AuthRouter.post(
+authRouter.post(
     "/signUp",
-    async (req: Request, res: Response, next: NextFunction) => {},
+    async (req: Request, res: Response, next: NextFunction) => {
+        const { name, email, password } = req.body
+        console.log(req.body)
+
+        if (email === "") {
+            res.statusCode = 400
+            res.send("email is empty")
+        } else if (name === "") {
+            res.statusCode = 400
+            res.send("name is empty")
+        } else if (password === "") {
+            res.statusCode = 400
+            res.send("password is empty")
+        } else {
+            res.sendStatus(200)
+        }
+    },
 )
-AuthRouter.post(
+authRouter.post(
     "/check",
     async (req: Request, res: Response, next: NextFunction) => {},
 )
 
-export default AuthRouter
+export default authRouter
