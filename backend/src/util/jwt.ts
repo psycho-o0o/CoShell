974cffe540d399ad981b3e2fa1e07f1993a9dd68
psycho-o0o <psycho-o0o@gmail.com>
2023-07-29 04:29:08 +0900

    @Feat/backend jwt validate와 generate 유틸함수

---
+++ backend/src/util/jwt.ts
@@ -0,0 +1,51 @@
+import {
+    sign,
+    SignOptions,
+    verify,
+    VerifyOptions,
+    Jwt,
+    JwtPayload,
+} from "jsonwebtoken"
+import * as fs from "fs"
+import * as path from "path"
+
+interface IPayload {
+    name?: string
+    email?: string
+}
+
+export function generateToken(payload: any) {
+    const privateKey = fs.readFileSync(
+        path.join(__dirname, "./../../private.key"),
+    )
+    const signInOptions: SignOptions = {
+        algorithm: "RS256",
+        expiresIn: "1h",
+    }
+
+    return sign(
+        payload,
+        { key: privateKey, passphrase: process.env.PASS_PHRASE as string },
+        signInOptions,
+    )
+}
+
+export function validateToken(
+    token: string,
+): Promise<Jwt | JwtPayload | string | undefined> {
+    const publicKey = fs.readFileSync(
+        path.join(__dirname, "./../../../public.key"),
+    )
+
+    const verifyOptions: VerifyOptions = {
+        algorithms: ["RS256"],
+    }
+
+    return new Promise((resolve, reject) => {
+        verify(token, publicKey, verifyOptions, (error, decoded) => {
+            if (error) return reject(error)
+
+            resolve(decoded)
+        })
+    })
+}
